1d9d5b15748437cc17507ba82e7a78aef26dd514
Glen Popowich <glenp@telus.net>
2025-10-07 08:16:06 -0700

    adjusted styles for the navbar so it is hidden when small and added hamburger menu

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+// Hamburger menu
+const hamburger = document.getElementById("hamburger");
+const overlay = document.getElementById("overlay");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("active");
+  overlay.classList.toggle("active");
+});
